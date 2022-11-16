d76697f0fe915932530810eec9b150fd1baa9582
James Hodgkinson <james@terminaloutcomes.com>
2022-11-16 11:44:19 +1000

    pushing better code

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use std::str::FromStr;
 
 use axum_server::tls_rustls::RustlsConfig;
 
-pub async fn get_tls_config() -> Result<RustlsConfig, std::io::Error> {
+pub async fn get_tls_config()  {
     let testval1 = PathBuf::from_str("/test").unwrap();
     let testval2 = PathBuf::from_str("/test").unwrap();
 
-    RustlsConfig::from_pem_file(testval1.to_owned(), testval2.clone()).await
+    let _ = RustlsConfig::from_pem_file(testval1.to_owned(), testval2.clone()).await;
 }
